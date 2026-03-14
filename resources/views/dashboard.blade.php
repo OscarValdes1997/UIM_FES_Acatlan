ea99418f7c0cb95f2a86409de0ed5baa1f195427
CoreAppMedia <coreAppMedia@gmail.com>
2026-03-14 14:57:07 -0600

    2
    2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>futbol Club Zapotitlan</title>
+    <title>Fes Acatlan</title>
     <link rel="icon" href="/favicon.ico">
 
     @viteReactRefresh
